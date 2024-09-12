d6d117fa993e11846c24e5a2e9828f4ea5583a31
BenHaeck <84250841+BenHaeck@users.noreply.github.com>
2024-09-12 15:25:35 -0400

    Update Board.cs
    Added comments

--- ConsoleMinesweeper/ConsoleMinesweeper/Board.cs
+++ ConsoleMinesweeper/ConsoleMinesweeper/Board.cs
@@ -7,17 +7,21 @@ using System.Threading.Tasks;
 namespace ConsoleMinesweeper {
 	public class Board {
 		Random random = new Random ();
-		readonly bool[,] bombs;
-		readonly bool[,] uncovered;
-		readonly bool[,] flaged;
+		readonly bool[,] bombs; // the bombs as a grid
+		readonly bool[,] uncovered; // whether the player has cleared a tile
+		readonly bool[,] flaged; // tracks where the player plants flags
+		// the bombs stored as integers. stored in an ordered list. This helps ensure that 2 bombs can't occupy the same tile
+		// each number has a corresponding tile. It goes from left to right, and moves down to the next row whenever it reaches the width of the board
 		List<int> bombLocations;
 		public readonly (int width, int height) boardSize;
 		public Board (int width, int height, int numBombs) {
+			// creates the 2d Arrays
 			bombs = new bool[height, width];
 			uncovered = new bool[height, width];
 			flaged = new bool[height, width];
 			boardSize = (width, height);
 
+			// creates the list of bombs
 			bombLocations = new List<int>();
 
 			for (int i = 0; i < numBombs; i++) {
@@ -30,6 +34,7 @@ namespace ConsoleMinesweeper {
 			}
 		}
 
+		// formats bombs from the int list to the 2d array
 		public void FormatBombs () {
 			for (int y = 0; y < boardSize.height; y++) {
 				for (int x = 0; x < boardSize.width; x++) {
@@ -42,19 +47,20 @@ namespace ConsoleMinesweeper {
 			}
 		}
 
+		// clears a S by S square around a tile. This is used to ensure that the player won't land on a mine in their first move
 		public void RerollLocations ((int x, int y) loc, int s) {
 			for (int j = 0; j <= 4; j++) {
 				for (int y = -s; y <= s; y++) {
-					if (0 > y+loc.y || y+loc.y >= boardSize.height) continue;
+					if (0 > y+loc.y || y+loc.y >= boardSize.height) continue; // skips anything outside the board
 					for (int x = -s; x <= s; x++) {
-						if (0 > x+loc.x || x+loc.x >= boardSize.width) continue;
+						if (0 > x+loc.x || x+loc.x >= boardSize.width) continue; // skips anything outside the board
 
 						for (int i = 0; i < bombLocations.Count; i++) {
-
+							// test for bombs
 							if (bombLocations[i] == ((loc.y+y)*boardSize.width + (loc.x+x))) {
-
+								// removes the bomb
 								bombLocations.RemoveAt (i);
-
+								// places the bomb somewhere else
 								RandomlyPlaceBomb ();
 								break;
 							}
@@ -62,14 +68,16 @@ namespace ConsoleMinesweeper {
 					}
 				}
 			}
-			FormatBombs ();
+			FormatBombs (); // updates the 2d bomb array
 		}
 
+		// places a bomb at a random location where there currently isn't a bomb
 		public int RandomlyPlaceBomb () {
 			return SafeInsert (random.Next (boardSize.width* boardSize.height - bombLocations.Count));
 		}
-
-		public int SafeInsert (int loc) {
+		// inserts a bomb somewhere there isn't a bomb already while keeping the bomb locations list
+		// as an ordered list
+		private int SafeInsert (int loc) {
 			for (int j = 0; j < bombLocations.Count; j++) {
 				if (bombLocations[j] <= loc) {
 					loc++;
@@ -86,15 +94,15 @@ namespace ConsoleMinesweeper {
 		}
 
 		
-
+		// returns how many bombs suround a tile
 		public int CountBombs ((int x, int y) location) {
 			int numBombs = 0;
 			for (int y = location.y - 1; y <= location.y + 1; y++) {
-				if (y < 0) continue;
-				if (y >= boardSize.height) break;
+				if (y < 0) continue;// skips tiles outside the board
+				if (y >= boardSize.height) break; // skips tiles outside the board
 				for (int x = location.x - 1; x <= location.x + 1; x++) {
-					if (x < 0) continue;
-					if (x >= boardSize.width) break;
+					if (x < 0) continue; // skips tiles outside the board
+					if (x >= boardSize.width) break; // skips tiles outside the board
 
 					numBombs += bombs[y, x] ? 1 : 0;
 				}
@@ -102,6 +110,7 @@ namespace ConsoleMinesweeper {
 			return numBombs;
 		}
 
+		// prints the board to the terminal
 		public void PrintBoard ((int x, int y) curser) {
 			for (int y = 0; y < boardSize.height; y++) {
 				for (int x = 0; x < boardSize.width; x++) {
@@ -132,6 +141,7 @@ namespace ConsoleMinesweeper {
 			}
 		}
 
+		// if the specified position is covered, place a flag there if there isn't one alfready, or remove one if theres already one there
 		public void ToggleFlag ((int x, int y) curser) {
 			if (!uncovered[curser.y, curser.x])
 				flaged[curser.y, curser.x] = !flaged[curser.y, curser.x];
@@ -140,13 +150,13 @@ namespace ConsoleMinesweeper {
 		
 
 		public void Uncover ((int x, int y) curser, bool initialClick) {
-			if (curser.x < 0 || curser.x >= boardSize.width) return;
+			if (curser.x < 0 || curser.x >= boardSize.width) return; // stops the funstion if the specified location isn't there
 			if (curser.y < 0 || curser.y >= boardSize.height) return;
 			if (flaged[curser.y, curser.x]) return;
 			if (!uncovered[curser.y, curser.x]) {
 				
-				uncovered[curser.y, curser.x] = true;
-				if (CountBombs (curser) <= 0) {
+				uncovered[curser.y, curser.x] = true;// clears a tile
+				if (CountBombs (curser) <= 0) { // clears large sections of bombless terrain.
 					for (int d = -1; d <= 1; d+=2) {
 						Uncover ((curser.x + d, curser.y), false);
 						Uncover ((curser.x, curser.y + d), false);
@@ -156,7 +166,7 @@ namespace ConsoleMinesweeper {
 				}
 				
 			}
-			else if (initialClick) {
+			else if (initialClick) { // clears a 3x3 area around the tile, if the tile is already clear.
 				for (int y = -1; y <= 1; y += 1) {
 					if (0 > y+curser.y || y+curser.y >= boardSize.height) continue;
 					for (int x = -1; x <= 1; x += 1) {
@@ -166,10 +176,13 @@ namespace ConsoleMinesweeper {
 			}
 		}
 
+		// returns if a tile is a bomb
 		public bool IsBomb ((int x, int y) loc) {
 			return bombs[loc.y, loc.x];
 		}
 
+			
+		// determains if the player one or lost the game by looking for tiles that are both uncovered and a bomb
 		public bool LostGame () {
 			for (int y = 0; y < boardSize.height; y++) {
 				for (int x = 0; x < boardSize.width; x++) {
@@ -179,6 +192,7 @@ namespace ConsoleMinesweeper {
 			return false;
 		}
 
+		// determines if the player has won the game by looking for tiles that are covered and not bombs
 		public bool WonGame () {
 			bool won = true;
 			for (int y = 0; y < boardSize.height; y++) {
